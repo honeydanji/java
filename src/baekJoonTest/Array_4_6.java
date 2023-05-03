60579d5a9550b140cbe0ba29ebb13924a1bc5a9e
honeydanji <tjdwls3680@naver.com>
2023-05-03 17:53:18 +0900

    d

--- src/baekJoonTest/Array_4_6.java
+++ src/baekJoonTest/Array_4_6.java
@@ -2,26 +2,30 @@ package baekJoonTest;
 
 import java.util.Scanner;
 
-public class Array_4_5 {
+public class Array_4_6 {
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		int N = sc.nextInt();
 		int M = sc.nextInt();
-
 		int[] A = new int[N];
-		for(int d = 1; d <= M; d++) {
+
+		for (int d = 0; d < A.length; d++) {
+			A[d] = d + 1;
+		}
+
+		for (int k = 0; k < M; k++) {
 			int i = sc.nextInt();
 			int j = sc.nextInt();
-			int k = sc.nextInt();
-
-			for (; i <= j; i++) {
-				A[i-1] = k;
-			}
+			int temp = A[i - 1];
+			A[i - 1] = A[j - 1];
+			A[j - 1] = temp;
 		}
-		
 		for (int a : A) {
 			System.out.print(a + " ");
 		}
+
 	}
+
 }
