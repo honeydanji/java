abfa24dae31dd65bae5f10647f2fd23c5345c696
honeydanji <tjdwls3680@naver.com>
2023-05-30 00:03:41 +0900

    study

--- src/Study/study5_Queue_1966.java
+++ src/Study/study5_Queue_1966.java
@@ -1,21 +1,44 @@
 package Study;
 
-import java.awt.Point;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class study5_Queue_1966 {
 	public static Scanner sc = new Scanner(System.in);
-	public static Queue<Integer> Q0, Q1, Q2 = new LinkedList<>();
+	public static Queue<int[]> Q0, Q1, Q2 = new LinkedList<>();
 
+	
+	 public static void sort(Integer[] array) {
+		 int n = array.length;
+	        boolean swapped;
+
+	        for (int i = 0; i < n - 1; i++) {
+	            swapped = false;
+	            for (int j = 0; j < n - i - 1; j++) {
+	                if (array[j] > array[j + 1]) {
+	                    // Swap array[j] and array[j + 1]
+	                    int temp = array[j];
+	                    array[j] = array[j + 1];
+	                    array[j + 1] = temp;
+	                    swapped = true;
+	                }
+	            }
+	            if (!swapped) {
+	                break;
+	            }
+	        }
+	    }
+	
 	public static void main(String args[]) {
 		// 테스트 케이스 수 입력 == 배열의 수
 		int n = sc.nextInt();
 		int[][] Test = new int[n][2];
-		int[] Test0 = null;
-		int[] Test1 = null;
-		int[] Test2 = null;
+		Integer[] Test0 = null;
+		Integer[] Test1 = null;
+		Integer[] Test2 = null;
 
 		for (int i = 0; i < Test.length; i++) {
 			for (int j = 0; j < Test[0].length; j++) {
@@ -23,13 +46,13 @@ public class study5_Queue_1966 {
 			}
 			switch (i) {
 			case 0:
-				Test0 = new int[Test[i][0]];
+				Test0 = new Integer[Test[i][0]];
 				break;
 			case 1:
-				Test1 = new int[Test[i][0]];
+				Test1 = new Integer[Test[i][0]];
 				break;
 			case 2:
-				Test2 = new int[Test[i][0]];
+				Test2 = new Integer[Test[i][0]];
 				break;
 			}
 			for (int k = 0; k < Test[i][0]; k++) {
@@ -46,58 +69,42 @@ public class study5_Queue_1966 {
 				}
 			}
 		}
-
-		for (int c = 0; c < Test1.length; c++) {
-			for (int v = 1; v < Test1.length; v++) {
-				if (Test1[c] < Test1[v]) {
-					int temp = Test1[v];
-					Test1[v] = Test1[c];
-					Test1[c] = temp;
-				}
-			}
+		
+		for(int a : Test0) {
+			System.out.print(a);
 		}
-
-//		for (int c = 0; c < Test1.length/2; c++) {
-//			if (Test1[c] < Test1[c+1]) {
-//				int temp = Test1[Test1.length - 1 - c];
-//				Test1[Test1.length - 1 - c] = Test1[c];
-//				Test1[c] = temp;
-//			}else
-//				continue;
-//		}
-
-//		for(int a : Test0) {
-//			System.out.print(a);
-//		}
-//		System.out.println();
+		System.out.println();
 		for (int a : Test1) {
 			System.out.print(a);
 		}
-//		System.out.println();
-//		for(int a : Test2) {
-//			System.out.print(a);
-//		}
-
-//		for (int i = 0; i < n; i++) {
-//			int[] TestCase = new int[sc.nextInt()];
-//			searchIndex = sc.nextInt();
-//			for (int j = 0; j < TestCase.length; j++) {
-//				TestCase[j] = sc.nextInt();
-//				Q.add(TestCase[j]);
-//			}
-//			while (true) {
-//				if (Q.poll() == TestCase[searchIndex]) {
-//					System.out.println(count);
-//					break;
-//				}else {
-//					Q.peek();
-//					count++;
-//				}
-//			}
-//		}
+		System.out.println();
+		for(int a : Test2) {
+			System.out.print(a);
+		}
+		
+		sort(Test0);
+		sort(Test1);
+		sort(Test2);
+		
+		System.out.println();
+		for(int a : Test0) {
+			System.out.print(a);
+		}
+		System.out.println();
+		for (int a : Test1) {
+			System.out.print(a);
+		}
+		System.out.println();
+		for(int a : Test2) {
+			System.out.print(a);
+		}
 
 	}
 }
+
+
+
+
 
 // Document : 테스트케이스의 수
 // N : 문서의 개수 (1 ~ 100)
