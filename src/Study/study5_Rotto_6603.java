4d5a34bc37b4e3f842f94b9bca26be1aa0f84bc5
honeydanji <tjdwls3680@naver.com>
2023-05-31 00:09:10 +0900

    내일은 꼭 풀자

--- src/Study/study5_Rotto_6603.java
+++ src/Study/study5_Rotto_6603.java
@@ -3,40 +3,60 @@ package Study;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class study5_Rotto_6603 {
 	static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 	static Scanner sc = new Scanner(System.in);
+	// static Random rd = new Random();
 
 	public static void main(String args[]) throws IOException {
-		//StringTokenizer st = new StringTokenizer(bf.readLine());
-		
-		// 집합 크기 설정 
-		//int n = Integer.parseInt(st.nextToken());
+		// StringTokenizer st = new StringTokenizer(bf.readLine());
+
+		// 집합 크기 설정
+		// int n = Integer.parseInt(st.nextToken());
 		int n = sc.nextInt();
-		
-		// 집합 생성 
-		List<Integer> list = new ArrayList<>();
-		int[] s = new int[list.size()];
-		
-		for (int i = 0; i < n; i++) {
-			list.add(sc.nextInt());
-			s[i] = list.get(i);
+		int[] s = new int[n];
+
+		for (int i = 0; i < s.length; i++) {
+			s[i] = sc.nextInt();
 		}
-		
-		List<Integer>[][] arr = new ArrayList[0][6];
-		
-		Arrays.sort(s);
-		
-		for(int j : s) {
-			System.out.println(s); 
+
+		for (int i = 0; i < 6; i++) {
+			int[] arr = new int[6];
+			for (int j = 0; j < 6; j++) {
+				arr[j] = ((int) Math.floor(Math.random() * s.length + 1));
+			}
+			LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>( Arrays.asList(arr) );
+			Arrays.sort(arr);
+			for (int a : arr) {
+				System.out.print(a + " ");
+			}
+			System.out.println();
 		}
+
+		// 집합 생성
+//		List<Integer> list = new ArrayList<>();
+//		int[] s = new int[list.size()];
+//		
+//		for (int i = 0; i < n; i++) {
+//			list.add(sc.nextInt());
+//			s[i] = list.get(i);
+//		}
+//		
+//		List<Integer>[][] arr = new ArrayList[0][6];
+//		
+//		Arrays.sort(s);
+//		
+//		for(int j : s) {
+//			System.out.println(s); 
+//		}
+		
+		
+		//참고
+		//https://seeminglyjs.tistory.com/247
 
 	}
 }
