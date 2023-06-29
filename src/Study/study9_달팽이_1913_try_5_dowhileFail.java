09c448b81d009610ed434d2d471bd803522e47de
honeydanji <tjdwls3680@naver.com>
2023-06-29 10:02:18 +0900

    try_4 성공

---
+++ src/Study/study9_달팽이_1913_try_5_dowhileFail.java
@@ -0,0 +1,90 @@
+package Study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 좌표 출력
+class PPPoint{
+	int x;
+	int y;
+
+	public void point(int x, int y) {
+		this.x = x+1;
+		this.y = y+1;
+	}
+}
+
+public class study9_달팽이_1913_try_5_dowhileFail {
+	public static void main(String args[]) throws NumberFormatException, IOException {
+		PPPoint p = new PPPoint();
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 배열크기
+		int N = Integer.parseInt(bf.readLine()); // N = 홀수
+		if(N < 3 || N > 999) bf.close();
+		
+		// 찾고 싶은 수
+		int num = Integer.parseInt(bf.readLine());
+		
+		// 달팽이배열
+		int[][] NN = new int[N][N];
+		
+		// 리스트에 값을 저장
+		int k = (int) Math.pow(N, 2);
+
+		int i = 0;
+		int j = 0;
+		
+		// k가 기준
+		while(k != 0) {
+			do { 
+				if(k == Math.pow(N,2)) NN[i][j] = k--; // i=0, j=0 인 경우만 해당한다.
+				else {
+					i++;
+					if(NN[i][j] != 0) { // 다음 칸에 값이 있는 지 없는 지 판단. 값이 존재하면 꺽는다. 
+						i--;
+						break;
+					}
+					NN[i][j] = k--;
+				}
+			}while(i < N);
+			do {
+				j++;
+				if(NN[i][j] != 0) {
+					j--;
+					break;
+				}
+				NN[i][j] = k--;
+			}while(j < N);
+			do {
+				i--;
+				if(NN[i][j] != 0) {
+					i++;
+					break;
+				}				
+				NN[i][j] = k--;
+				}while(i != 0);
+			do{
+				j--;
+				if(j < 0 || NN[i][j] != 0) {
+					j++;
+					break;
+				}
+				NN[i][j] = k--;	
+			}while(j != 0); 
+		}
+		
+		for(int ii = 0; ii < NN.length; ii++) {
+			for(int jj = 0; jj < NN.length; jj++) {
+				System.out.print(NN[ii][jj] + " ");
+				if(NN[ii][jj] == num) {
+					p.point(ii, jj);
+				}
+			}
+			System.out.println();
+		}		
+		System.out.println(p.x + " " + p.y);
+	}
+}
+
