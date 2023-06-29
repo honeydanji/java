da2878e7d95f978edb141fa560c16f927eb875ef
honeydanji <tjdwls3680@naver.com>
2023-06-29 10:22:34 +0900

    최종판

--- src/Study/study9_달팽이_1913_try_4.java
+++ src/Study/study9_달팽이_1913_try_4.java
@@ -1,8 +1,10 @@
 package Study;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 // 좌표 출력
 class PPPoint{
@@ -19,13 +21,14 @@ public class study9_달팽이_1913_try_4 {
 	public static void main(String args[]) throws NumberFormatException, IOException {
 		PPPoint p = new PPPoint();
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		// 배열크기
 		int N = Integer.parseInt(bf.readLine()); // N = 홀수
 		if(N < 3 || N > 999) bf.close();
-		
 		// 찾고 싶은 수
 		int num = Integer.parseInt(bf.readLine());
+		bf.close();
 		
 		// 달팽이배열
 		int[][] NN = new int[N][N];
@@ -77,14 +80,17 @@ public class study9_달팽이_1913_try_4 {
 		
 		for(int ii = 0; ii < NN.length; ii++) {
 			for(int jj = 0; jj < NN.length; jj++) {
-				System.out.print(NN[ii][jj] + " ");
+				bw.write(String.valueOf(NN[ii][jj]));
+				bw.write(" ");
 				if(NN[ii][jj] == num) {
 					p.point(ii, jj);
 				}
 			}
-			System.out.println();
-		}		
-		System.out.println(p.x + " " + p.y);
+			bw.newLine();
+		}
+		bw.write(String.valueOf(p.x) + " " + String.valueOf(p.y));
+		bw.flush();
+		bw.close();
 	}
 }
 
